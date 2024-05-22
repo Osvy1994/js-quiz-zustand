7e866a494546e92da249f1795ffdd8dcb51cbc78
Osvaldo Madrigal <osvaldo.elvirez@gmail.com>
2024-05-22 15:14:57 -0400

    implemented persist middleware

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { type Question } from '../types'
 import confetti from 'canvas-confetti'
-import { devtools } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 
 interface State {
   questions: Question[]
@@ -15,54 +15,61 @@ interface State {
 
 export const useQuestionsStore = create<State>()(
   devtools(
-    (set, get) => {
-      return {
-        questions: [],
-        currentQuestion: 0,
-        fetchQuestions: async (limit: number) => {
-          const res = await fetch('http://localhost:5173/data.json')
-          const json = await res.json()
+    persist(
+      (set, get) => {
+        return {
+          questions: [],
+          currentQuestion: 0,
+          fetchQuestions: async (limit: number) => {
+            const res = await fetch('http://localhost:5173/data.json')
+            const json = await res.json()
 
-          const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
-          set({ questions }, false, 'FETCH_QUESTIONS')
-        },
-        selectAnswer: (questionId: number, answerIndex: number) => {
-          const state = get()
-          const newQuestions = structuredClone(state.questions)
-          const questionIndex = newQuestions.findIndex(q => q.id === questionId)
-          const questionInfo = newQuestions[questionIndex]
-          const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
-          if (isCorrectUserAnswer) confetti()
-          newQuestions[questionIndex] = {
-            ...questionInfo,
-            isCorrectUserAnswer,
-            userSelectedAnswer: answerIndex,
-          }
-          set({ questions: newQuestions }, false, 'SELECT_ANSWER')
-        },
-        goNextQuestion: () => {
-          const { currentQuestion, questions } = get()
-          const nextQuestion = currentQuestion + 1
+            const questions = json
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit)
+            set({ questions }, false, 'FETCH_QUESTIONS')
+          },
+          selectAnswer: (questionId: number, answerIndex: number) => {
+            const state = get()
+            const newQuestions = structuredClone(state.questions)
+            const questionIndex = newQuestions.findIndex(
+              q => q.id === questionId,
+            )
+            const questionInfo = newQuestions[questionIndex]
+            const isCorrectUserAnswer =
+              questionInfo.correctAnswer === answerIndex
+            if (isCorrectUserAnswer) confetti()
+            newQuestions[questionIndex] = {
+              ...questionInfo,
+              isCorrectUserAnswer,
+              userSelectedAnswer: answerIndex,
+            }
+            set({ questions: newQuestions }, false, 'SELECT_ANSWER')
+          },
+          goNextQuestion: () => {
+            const { currentQuestion, questions } = get()
+            const nextQuestion = currentQuestion + 1
 
-          if (nextQuestion < questions.length) {
-            set({ currentQuestion: nextQuestion }, false, 'NEXT_QUESTION')
-          }
-        },
-        goPreviousQuestion: () => {
-          const { currentQuestion } = get()
-          const nextQuestion = currentQuestion - 1
+            if (nextQuestion < questions.length) {
+              set({ currentQuestion: nextQuestion }, false, 'NEXT_QUESTION')
+            }
+          },
+          goPreviousQuestion: () => {
+            const { currentQuestion } = get()
+            const nextQuestion = currentQuestion - 1
 
-          if (nextQuestion >= 0) {
-            set({ currentQuestion: nextQuestion }, false, 'PREVIOUS_QUESTION')
-          }
-        },
-        resetGame: () => {
-          set({ questions: [], currentQuestion: 0 }, false, 'RESET')
-        },
-      }
-    },
-    {
-      name: 'questions',
-    },
+            if (nextQuestion >= 0) {
+              set({ currentQuestion: nextQuestion }, false, 'PREVIOUS_QUESTION')
+            }
+          },
+          resetGame: () => {
+            set({ questions: [], currentQuestion: 0 }, false, 'RESET')
+          },
+        }
+      },
+      {
+        name: 'questions',
+      },
+    ),
   ),
 )
